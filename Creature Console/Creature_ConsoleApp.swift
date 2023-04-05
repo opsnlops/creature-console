deb6fbb607409b4ed5c00b9ab3af60f20772878e
April White <april@opsnlops.io>
2023-04-04 19:51:54 -0700

    First commit

---
+++ Creature Console/Creature_ConsoleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Creature_ConsoleApp.swift
+//  Creature Console
+//
+//  Created by April White on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Creature_ConsoleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
